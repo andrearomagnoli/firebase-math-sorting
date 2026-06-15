4bf74915f9215c60534c6596cc552fa223ff4156
Andrea <romagnoli.music@gmail.com>
2026-06-15 16:48:04 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.database();
